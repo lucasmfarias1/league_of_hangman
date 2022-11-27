6462773942b804b75e132b2915755a63608d76cf
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2022-11-27 16:50:58 -0300

    cleanup, install bootstrap

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,6 +1,4 @@
 class GuessesController < ApplicationController
-  before_action :validate_guess, only: [:create]
-
   def show
     set_spell
     if @spell.blank?
@@ -9,8 +7,6 @@ class GuessesController < ApplicationController
     end
 
     session[:beat_ids] ||= []
-    session[:guesses] = []
-    session[:correct_guesses] = 0
     session[:spellname] = @spell.name.upcase
     session[:spell_id] = @spell.id
   end
@@ -21,20 +17,6 @@ class GuessesController < ApplicationController
   end
 
   private
-
-  def validate_guess
-    render :create_failure unless params[:guess].present? &&
-                                  guess.length == 1 &&
-                                  guess.match?(/[[:alpha:]]/)
-  end
-
-  def guess
-    params[:guess].upcase
-  end
-
-  def name_complete
-    session[:correct_guesses] >= session[:spellname].gsub(/[^a-z]/i, '').chars.uniq.size
-  end
 
   def set_spell
     @spell = Spell.where(
