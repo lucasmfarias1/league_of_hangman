db3da82f1f753a08942992239abcede42d229ee3
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2022-11-29 12:55:47 -0300

    switches to cookies; improves loader;

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -4,7 +4,7 @@ class GuessesController < ApplicationController
   before_action :set_player
 
   def show
-    session[:start_time] ||= Time.now
+    cookies[:start_time] ||= { :value => Time.now, :expires => 99.days.from_now }
 
     return render_game_over if @player.beat_spells.count >= 648
 
@@ -18,9 +18,8 @@ class GuessesController < ApplicationController
 
   def reset
     @player.beat_spells.destroy_all
-    session[:start_time] = Time.now
-    session.delete(:end_time)
-    session.delete(:beat_ids)
+    cookies[:start_time] = Time.now
+    cookies.delete(:end_time)
 
     redirect_to guesses_path
   end
@@ -35,13 +34,13 @@ class GuessesController < ApplicationController
   end
 
   def render_game_over
-    session[:end_time] ||= Time.now
-    @time_to_complete = distance_of_time_in_words(session[:start_time], session[:end_time])
+    cookies[:end_time] ||= { :value => Time.now, :expires => 99.days.from_now }
+    @time_to_complete = distance_of_time_in_words(cookies[:start_time], cookies[:end_time])
     render :game_over
   end
 
   def set_player
-    @player = Player.find_by(id: session[:player_id]) || Player.create
-    session[:player_id] = @player.id
+    @player = Player.find_by(id: cookies[:player_id]) || Player.create
+    cookies[:player_id] ||= { :value => @player.id, :expires => 99.days.from_now }
   end
 end
--- lib/loader.rb
+++ lib/loader.rb
@@ -5,11 +5,12 @@ champion_names = parsed['data'].keys
 champion_names.each do |name|
   response = Net::HTTP.get(URI("http://ddragon.leagueoflegends.com/cdn/12.22.1/data/en_US/champion/#{name}.json"))
   parsed = JSON.parse(response)
+  real_name = parsed['data'][name]['name']
   spells = parsed['data'][name]['spells']
 
   spells.each.with_index do |spell, i|
-    Spell.create(
-      champion: name,
+    Spell.where(name: spell['name']).update(
+      champion: real_name,
       name: spell['name'],
       cooldown: spell['cooldown'].join('/'),
       cost: spell['cost'].join('/'),
